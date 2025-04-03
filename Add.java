934b7e773f545fd26ed5fb5635d0ccfc61251982
dev1 <dev1@demo.lab>
2025-04-03 15:41:49 +0530

    code changes done

--- Add.java
+++ Add.java
@@ -2,9 +2,9 @@ import java.util.*;
 class Add{
 		public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter num1);
+		System.out.println("Enter num1");
 		int num1=sc.nextInt();
-		System.out.println("Enter num2);
+		System.out.println("Enter num2");
 		int num2=sc.nextInt();
 		int add=num1+num2;
 		System.out.println("Sum is : "+add);
