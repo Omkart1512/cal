167aa2886b570fa57602d82d3ed9ef879cfb08af
dev1 <dev1@demo.lab>
2025-04-03 15:20:13 +0530

    addtion done

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Add{
+		public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter num1);
+		int num1=sc.nextInt();
+		System.out.println("Enter num2);
+		int num2=sc.nextInt();
+		int add=num1+num2;
+		System.out.println("Sum is : "+add);}
+}
